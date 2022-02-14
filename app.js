bb8a57f8a72f971fb484ed7842826eb6e65eb9d8
mdrubel02 <mdrubel072125@gmail.com>
2022-02-14 18:57:35 +0600

    added

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-document.getElementById('incrice-btn').addEventListener('click',function(){
+
+function mobilePrice( simple ){
+    const mobileInput=document.getElementById('phone-number');
+    console.log(mobileInput);
+    let mobileInputText=mobileInput.value;
+    if(simple == true){
+        mobileInputText=parseInt(mobileInputText) + 1;
+        console.log(mobileInput);
+    }
+    else if(mobileInputText  > 0){
+        mobileInputText=parseInt(mobileInputText) - 1;
+    }
+    mobileInput.value=mobileInputText;
+    const phonePrice=document.getElementById('mobile-price')
+    phonePrice.innerText=mobileInputText * 1213;
+};
+
+document.getElementById('phone-plus').addEventListener('click',function(){
     mobilePrice(true);
+    // const mobileInput=document.getElementById('mobile-input');
+    // const mobileInputText=mobileInput.value;
+    // mobileInput.value=parseInt(mobileInputText) + 1;
 });
-document.getElementById('discriment-btn').addEventListener('click', function(){
+document.getElementById('phone-minus').addEventListener('click', function(){
     console.log('clicked')
     mobilePrice(false);
 });
-
-
-function mobilePrice( simple ){
-    const mobileInput=document.getElementById('mobile-input');
-    console.log(mobileInput);
-    const mobileInputText=mobileInput.value;
-    if(simple == true){
-        mobileInputText.value= mobileInput.value + 1;
-        console.log(mobileInput.value);
-    }
-} 
