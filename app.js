7c5d44681b3f853fbde070a81e84ba755137e3ed
mdrubel02 <mdrubel072125@gmail.com>
2022-02-14 15:07:25 +0600

    added

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello apple mobile'); 
+document.getElementById('incrice-btn').addEventListener('click',function(){
+    mobilePrice(true);
+});
+document.getElementById('discriment-btn').addEventListener('click', function(){
+    console.log('clicked')
+    mobilePrice(false);
+});
+
+
+function mobilePrice( simple ){
+    const mobileInput=document.getElementById('mobile-input');
+    console.log(mobileInput);
+    const mobileInputText=mobileInput.value;
+    if(simple == true){
+        mobileInputText.value= mobileInput.value + 1;
+        console.log(mobileInput.value);
+    }
+} 
